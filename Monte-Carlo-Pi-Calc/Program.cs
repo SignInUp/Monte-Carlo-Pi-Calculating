a12ceffde1c65bc697fac802457e6419c8979728
SingInUp <itemfouund@gmail.com>
2019-10-03 17:51:55 +0300

    Finished program xD

---
+++ Monte-Carlo-Pi-Calc/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using Shapes;
+
+namespace Monte_Carlo_Pi_Calc {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Radius = ");
+            var radius = int.Parse(Console.ReadLine());
+
+            if (radius < 10) {
+                Console.WriteLine("You has to write >= 10");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("There is " + (radius * radius) + " points");
+
+            Console.Write("Count of created points = ");    
+            var pointsCount = int.Parse(Console.ReadLine());
+
+            var stopwatch = Stopwatch.StartNew();
+            double[] pi = FindPi(pointsCount, radius);
+            stopwatch.Stop();
+
+            WriteData(pi, radius, pointsCount, stopwatch.ElapsedMilliseconds, "..\\..\\..\\Output.txt");
+
+            Console.ReadLine();
+        }
+        static double[] FindPi(int pointsCount, int radius) {
+            
+            var allPi = new double[10];
+            var circle = new Circle(radius);
+            var rnd = new Random();
+            var inCircle = 0;
+            
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < pointsCount / 10; ++j) {
+
+                    var point = new Point(rnd.Next(0, circle.Radius + 1), rnd.Next(0, circle.Radius + 1));
+
+                    if (circle.IsPointInCircle(point))
+                        ++inCircle;
+
+                }
+                allPi[i] = (4.0 * inCircle / pointsCount);
+            }
+
+            return allPi;
+        }
+        static void WriteData(double[] pi, int radius, int pointsCount, long time, string path) {
+
+            var fs = new StreamWriter(path, true);
+            fs.WriteLine("Radius = " + radius);
+            fs.WriteLine("Count of created points: " + pointsCount + "\r\n");
+
+            for (int i = 0; i < 10; ++i) {
+                fs.WriteLine("Cycle " + (i + 1) * (pointsCount / 10) + ": " + pi[i]);
+            }
+
+            fs.WriteLine("\r\nDelta: " + (Math.PI - pi[9]));
+            fs.WriteLine("TIME: " + time + "ms");
+            fs.WriteLine("<------------------------------------------>\r\n");
+            fs.Close();
+
+            Console.WriteLine("Data in the file by adress: " + path);
+
+        }
+    }
+}
---
+++ Monte-Carlo-Pi-Calc/Shapes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Shapes {
+    public class Circle {
+
+        public int Radius { get; private set; }
+        public Circle(int Radius) =>
+            this.Radius = Radius;
+
+        public bool IsPointInCircle(Point point) =>
+            Radius > Math.Sqrt(point.X * point.X + point.Y * point.Y);
+
+    }
+    public class Point {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Point(int X, int Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+        public override string ToString() => X + "_" + Y;
+    }
+}
